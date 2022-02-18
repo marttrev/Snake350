0869a8d28bfe0bc212e56634b1ce9185be15ef6c
Trevor Martin <tcmartin5000@gmail.com>
2022-02-17 23:41:27 -0500

    Capable of producing food visual. No logic, using magic numbers.

--- src/com/company/SnakePanel.java
+++ src/com/company/SnakePanel.java
@@ -11,10 +11,33 @@ public class SnakePanel extends JPanel {
     private static final int HEIGHT = 1024;
     private static final int DIVISION = 32;
     private static final int AREA = ((WIDTH * HEIGHT) / DIVISION);
+    // Temporary, will likely be accessed by method later:
+    private int foodXCoord;
+    private int foodYCoord;
 
     public SnakePanel() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    }
 
+    public void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        graphic(graphics);
+    }
+
+    public void graphic(Graphics graphics) {
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(getFoodXCoord(), getFoodYCoord(), DIVISION, DIVISION);
+    }
+
+    // Temporary, will be found in other class later
+    private int getFoodXCoord() {
+        foodXCoord = 768;
+        return foodXCoord;
+    }
+
+    private int getFoodYCoord() {
+        foodYCoord = 768;
+        return foodYCoord;
     }
 
 
