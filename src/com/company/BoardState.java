2457a9e76afa67ddadeab85ad4fc3726a1543bcf
Raunak03 <71651121+Raunak03@users.noreply.github.com>
2022-02-22 13:56:53 -0500

    Ro made changes to BoardState

--- src/com/company/BoardState.java
+++ src/com/company/BoardState.java
@@ -1,5 +1,38 @@
 package com.company;
 
+
 public enum BoardState {
     empty, occupiedBySnake, occupiedByFood, wall
+}
+
+class Board {
+    private static boolean emptyCell = getemptyCell();
+    private static boolean occupiedsnakeCell = getoccupiedsnakCell();
+    private static boolean occupiedfoodCell = getoccupiedfoodCell();
+    private static boolean wallCell = getwallCell();
+
+    //returns ture if the cell is occipied by the wall
+    private static boolean getwallCell() {
+        BoardState wallCell = BoardState.wall;
+        return false;
+    }
+
+    //returns true is the board is occupied by the the food
+    private static boolean getoccupiedfoodCell() {
+        BoardState foodCell = BoardState.occupiedByFood;
+        return false;
+    }
+
+    //return true is the cell is occupied by the snake
+    private static boolean getoccupiedsnakCell() {
+        BoardState snakeCell = BoardState.occupiedBySnake;
+        return false;
+
+    }
+    //return ture if the cell is empty
+    private static boolean getemptyCell() {
+        BoardState emptyCell = BoardState.empty;
+        return false;
+    }
+
 }
