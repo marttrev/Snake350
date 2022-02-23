10e94119261497677e477412d6a37c97f132b266
Trevor Martin <tcmartin5000@gmail.com>
2022-02-23 00:50:32 -0500

    Snake draws to a fixed size using placeholder methods. Awaiting backend implementation.

--- src/com/company/SnakePanel.java
+++ src/com/company/SnakePanel.java
@@ -11,6 +11,10 @@ public class SnakePanel extends JPanel {
     private static final int HEIGHT = getGridHeight();
     private static final int DIVISION = 32;
     private static final int AREA = ((WIDTH * HEIGHT) / DIVISION);
+    // The following variables are temporary, will be found in other classes later
+    private SnakeNode head;
+    private SnakeNode second;
+    private SnakeNode tail;
 
     public SnakePanel() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
@@ -22,8 +26,18 @@ public class SnakePanel extends JPanel {
     }
 
     public void graphic(Graphics graphics) {
+        // Draw food on screen
         graphics.setColor(Color.BLACK);
         graphics.fillRect(getFoodXCoord()*DIVISION, getFoodYCoord()*DIVISION, DIVISION, DIVISION);
+
+        // Draw snake on screen
+        /* This call is temporary */ generateSnake();
+        SnakeNode snake = getSnakeHead();
+        while (snake != null) {
+            graphics.setColor(Color.BLACK);
+            graphics.fillRect(snake.getXcoord()*DIVISION, snake.getYcoord()*DIVISION, DIVISION, DIVISION);
+            snake = snake.getNext();
+        }
     }
 
     // These private gets are temporary, will be found in other classes later
@@ -41,6 +55,25 @@ public class SnakePanel extends JPanel {
 
     private static int getGridHeight() {
         return 768;
+    }
+
+    private void generateSnake() {
+        head = new SnakeNode(4, 4);
+        second = new SnakeNode(4, 5);
+        tail = new SnakeNode(5, 5);
+
+        head.setNext(second);
+        second.setPrevious(head);
+        second.setNext(tail);
+        tail.setPrevious(second);
+    }
+
+    private SnakeNode getSnakeHead() {
+        return head;
+    }
+
+    private SnakeNode getSnakeTail() {
+        return tail;
     }
 
 
