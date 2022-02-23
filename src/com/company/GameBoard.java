40769706e10740e9abe0b86bca902cabc0a4ab04
Trevor Martin <tcmartin5000@gmail.com>
2022-02-23 00:52:25 -0500

    Added the SnakeNodes described in the spec document because the SnakeNode class now exists. Also added get methods.

--- src/com/company/GameBoard.java
+++ src/com/company/GameBoard.java
@@ -1,5 +1,15 @@
 package com.company;
 
 public class GameBoard {
+    SnakeNode snakeHead;
+    SnakeNode snakeTail;
+
+    public SnakeNode getSnakeHead() {
+        return snakeHead;
+    }
+
+    public SnakeNode getSnakeTail() {
+        return snakeTail;
+    }
 
 }
