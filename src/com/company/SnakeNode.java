c9f1f61b7bb96f94a300fe99a62b3cfcc85f1ed5
Lucas Champoux <champouxlucas@gmail.com>
2022-02-23 10:17:49 -0500

    created SnakeNode functionality. Acts as LinkedList structure of body segments

--- src/com/company/SnakeNode.java
+++ src/com/company/SnakeNode.java
@@ -6,12 +6,44 @@ public class SnakeNode {
 
     private int xcoord;
     private int ycoord;
+    private int direction; //0 = North, 1 = East, 2 = South, 3 = West
     private SnakeNode next;
     private SnakeNode previous;
+    private boolean isHead;
+    private boolean isTail;
+
 
     public SnakeNode(int xcoord, int ycoord) {
         this.xcoord = xcoord;
         this.ycoord = ycoord;
+        this.direction = 0;
+        isHead = false;
+        isTail = false;
+    }
+
+    public SnakeNode(int xcoord, int ycoord, boolean mode){
+        this.xcoord = xcoord;
+        this.ycoord = ycoord;
+        this.direction = 0;
+
+        if(mode){
+            isHead = true;
+            isTail = false;
+        }
+        else{
+            isHead = false;
+            isTail = true;
+        }
+    }
+
+    public SnakeNode(SnakeNode original){
+        this.xcoord = original.getXcoord();
+        this.ycoord = original.getYcoord();
+        this.direction = original.getDirection();
+        this.next = original.getNext();
+        this.previous = original.getPrevious();
+        this.isHead = this.isHead();
+        this.isTail = this.isTail();
     }
 
     public int getXcoord() {
@@ -44,5 +76,56 @@ public class SnakeNode {
 
     public void setPrevious(SnakeNode previous) {
         this.previous = previous;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public boolean isHead() {
+        return isHead;
+    }
+
+    public void setHead(boolean head) {
+        isHead = head;
+    }
+
+    public boolean isTail() {
+        return isTail;
+    }
+
+    public void setTail(boolean tail) {
+        isTail = tail;
+    }
+
+    public void linkNodeBehind(SnakeNode newNode){
+        this.next = newNode;
+        this.next.previous = this;
+    }
+
+    public void linkNodeFront(SnakeNode newNode){
+        newNode.next = this;
+        this.previous.next = newNode;
+    }
+
+    public void addNewSegment(){
+        SnakeNode currentNode = this;
+        do {
+            currentNode = currentNode.next;
+        } while (!currentNode.isTail());
+
+        SnakeNode newNode = new SnakeNode(currentNode);
+
+        currentNode.setNext(newNode);
+        newNode.setPrevious(currentNode);
+
+        currentNode.setTail(false);
+        newNode.setTail(true);
+
+
     }
 }
